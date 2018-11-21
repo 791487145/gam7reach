816c85dde6efdfa8505c6444514f902aaf0aa0ba
fengxin <502251245@qq.com>
2018-11-21 09:25:46 +0800

    2018 11 21 code

--- app/Http/Controllers/BaiscController.php
+++ app/Http/Controllers/BaiscController.php
@@ -14,20 +14,24 @@ class BaiscController extends BascController
 {
 
     const LIMIT=10;//每页条数
-    public $company_id;
-    public $admin = 1;
-    public $region = 2;
-    public $shoper = 3;
-    public $guide = 4;
-
+    public $company_id;//企业id
+    public $admin = 1;//超级管理员
+    public $region = 2;//区经
+    public $shoper = 3;//店长
+    public $guide = 4;//导购
+    public $store_id;//门店id
     public function __construct()
     {
         $this->middleware(function ($request, $next) {
             $name = Route::currentRouteName();
 
             $employ = auth('employ')->user();
+            if(empty($employ)){
+                return $this->failed('token过期或不正确');
+            }
             //dd($employ);
             $this->company_id = $employ->company_id;
+            $this->store_id = $employ->commpany_id;
             return $next($request);
         });
     }
