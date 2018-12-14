a00099d09fb24365fda4dda1e61f3e56915cb675
fengxin <502251245@qq.com>
2018-12-14 14:49:14 +0800

    web hook

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -29,10 +29,12 @@ class Controller extends BaseController
             $payloadHash = hash_hmac($algo, $json, $secret);
             if ($hash === $payloadHash) {
                 $cmd = "cd $target && git pull origin master";
-                $res = shell_exec($cmd);
-                $res_log=$content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：';
-                Log::channel('webhook')->info($res_log);
-                return response()->json(['status'=>'ok','code'=>'200']);
+                if($res = shell_exec($cmd)){
+                    $res_log=$content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支push了' . count($content['commits']) . '个commit：';
+                    Log::channel('webhook')->info($res_log);
+                    return response()->json(['status'=>'ok','code'=>'200']);
+                }
+                throw new \Exception('执行错误');
             }
         }catch (\Exception $e){
             Log::channel('webhook')->error($e->getMessage());
